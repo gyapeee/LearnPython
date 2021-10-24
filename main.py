7cf49f4afdc4280286b88e94ff0b525c34f3862f
gyapeee <gyapeee@gmail.com>
2021-10-24 19:34:40 +0200

    selenium scroll is added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import time
 from enum import Enum
-
+from lxml import html
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 TELETAL_URL = "https://www.teletal.hu/etlap/45"
@@ -20,32 +23,33 @@ class Workdays(Enum):
 # page = requests.get(url=TELETAL_URL)
 # decode bytes of page's content
 # document = html.fromstring(page.content.decode('utf-8'))
-browser = webdriver.Chrome(ChromeDriverManager().install())
+options = Options()
+options.headless = True
+browser = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 browser.get(TELETAL_URL)
-
+body = browser.find_element(By.TAG_NAME, 'body')
+body.send_keys(Keys.END)
 html_page = browser.page_source
 time.sleep(2)
-print(type(html_page))
-print(html_page)
-#
-# document = html.fromstring(page.content.decode('utf-8'))
-#
-# # extract days, meals and prices which contains csirkemell
-# days = document.xpath(PREFIX + "/div" + CSIRKEMELL + "following-sibling::div/a/@nap")
-# ingredients = document.xpath(PREFIX + CSIRKEMELL + "div/text()")
-# prices = document.xpath(PREFIX + CSIRKEMELL + "child::div[contains(@class,'menu-price-field')]/div/h6/strong/text()")
-#
-# # initialize the output
-# cheapest_csirkmell_at_weekdays = {day.name: ['Nincs', 'Nincs'] for day in Workdays}
-# # create a generator for the merged days, meals and prices
-# meals = ((Workdays(int(day)).name, ingredient, price) for day, ingredient, price in zip(days, ingredients, prices))
-# # Add cheaper meal to each day
-# for meal in meals:
-#     # replace the initial data if the actual price is less
-#     if cheapest_csirkmell_at_weekdays[meal[0]][1] > meal[2]:
-#         cheapest_csirkmell_at_weekdays[meal[0]][0] = meal[1]
-#         cheapest_csirkmell_at_weekdays[meal[0]][1] = meal[2]
-#
-# print(cheapest_csirkmell_at_weekdays)
+
+document = html.fromstring(html_page)
+
+# extract days, meals and prices which contains csirkemell
+days = document.xpath(PREFIX + "/div" + CSIRKEMELL + "following-sibling::div/a/@nap")
+ingredients = document.xpath(PREFIX + CSIRKEMELL + "div/text()")
+prices = document.xpath(PREFIX + CSIRKEMELL + "child::div[contains(@class,'menu-price-field')]/div/h6/strong/text()")
+
+# initialize the output
+cheapest_csirkmell_at_weekdays = {day.name: ['Nincs', 'Nincs'] for day in Workdays}
+# create a generator for the merged days, meals and prices
+meals = ((Workdays(int(day)).name, ingredient, price) for day, ingredient, price in zip(days, ingredients, prices))
+# Add cheaper meal to each day
+for meal in meals:
+    # replace the initial data if the actual price is less
+    if cheapest_csirkmell_at_weekdays[meal[0]][1] > meal[2]:
+        cheapest_csirkmell_at_weekdays[meal[0]][0] = meal[1]
+        cheapest_csirkmell_at_weekdays[meal[0]][1] = meal[2]
+
+print(cheapest_csirkmell_at_weekdays)
 
 browser.close()
