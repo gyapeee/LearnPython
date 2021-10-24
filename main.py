cdb86f46a5508fad28a7cccaf516330ab2c9e301
gyapeee <gyapeee@gmail.com>
2021-10-24 17:16:57 +0200

    cheapest csirkemell is printed

--- main.py
+++ main.py
@@ -3,29 +3,37 @@ from enum import Enum
 import requests
 from lxml import html
 
+PREFIX = "//div[contains(@class,'menu-card-5-day')]/div[contains(@class,'menu-cell-text')]"
+CSIRKEMELL = "[contains(normalize-space(),'csirkemell')]/"
+
 
 class Workdays(Enum):
-    MONDAY = 1
-    TUESDAY = 2
-    WEDNESDAY = 3
-    THURSDAY = 4
-    FRIDAY = 5
+    HÉTFŐ = 1
+    KEDD = 2
+    SZERDA = 3
+    CSÜTÖRTÖK = 4
+    PÉNTEK = 5
 
 
-# more days
-
+# get the menu from the url
 page = requests.get(url='https://www.teletal.hu/etlap/45')
 # decode bytes of page's content
-content = html.fromstring(page.content.decode('utf-8'))
+document = html.fromstring(page.content.decode('utf-8'))
 
-# find the index of the minimum in list https://www.kite.com/python/answers/how-to-find-the-index-of-the-minimum-value-of-a-list-in-python
+# extract days, meals and prices which contains csirkemell
+days = document.xpath(PREFIX + "/div" + CSIRKEMELL + "following-sibling::div/a/@nap")
+ingredients = document.xpath(PREFIX + CSIRKEMELL + "div/text()")
+prices = document.xpath(PREFIX + CSIRKEMELL + "child::div[contains(@class,'menu-price-field')]/div/h6/strong/text()")
 
-prefix = "//div[contains(@class,'menu-card-5-day')]/div[contains(@class,'menu-cell-text')]"
-csirkemell = "[contains(normalize-space(),'csirkemell')]/"
+# initialize the output
+cheapest_csirkmell_at_weekdays = {day.name: ['Nincs', 'Nincs'] for day in Workdays}
+# create a generator for the merged days, meals and prices
+meals = ((Workdays(int(day)).name, ingredient, price) for day, ingredient, price in zip(days, ingredients, prices))
+# Add cheaper meal to each day
+for meal in meals:
+    # replace the initial data if the actual price is less
+    if cheapest_csirkmell_at_weekdays[meal[0]][1] > meal[2]:
+        cheapest_csirkmell_at_weekdays[meal[0]][0] = meal[1]
+        cheapest_csirkmell_at_weekdays[meal[0]][1] = meal[2]
 
-prices = content.xpath(prefix + csirkemell + "child::div[contains(@class,'menu-price-field')]/div/h6/strong/text()")
-days = content.xpath(prefix + "/div" + csirkemell + "following-sibling::div/a/@nap")
-contents = content.xpath(prefix + csirkemell + "div/text()")
-
-meal_dict = [(Workdays(int(day)).name, content, price) for day, content, price in zip(days, contents, prices)]
-print(meal_dict)
+print(cheapest_csirkmell_at_weekdays)
